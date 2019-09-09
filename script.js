cc753f5e364781d4aca19f4630b89ddea0d6e5f9
shockedRose <miguelj0916@gmail.com>
2019-09-08 20:18:33 -0500

    tercer aporte, agregado formulario de creacion de nuevos conceptos

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const Concept = domString => new DOMParser().parseFromString(domString, 'text/html').body.firstChild;
 
-const concept = Concept('<section class="concept"><h4 class="title">1) Defina que es Moodle</h4><textarea class="textbox"></textarea><button class="save">Save</button></section>');
+const conceptsContainer = document.querySelector('.allConcepts');
+
+const button = document.querySelector('input[type="button"]');
+
+const text = document.querySelector('input[type="text"]');
+
+button.addEventListener('click', () => {
+    let newConcept = Concept(`<section class="concept"><h4 class="title">${text.value}</h4><textarea class="textbox"></textarea><button class="save">Save</button></section>`);
+
+    conceptsContainer.insertAdjacentElement('beforeend', newConcept);
+
+});
 
