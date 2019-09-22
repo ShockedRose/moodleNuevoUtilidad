75c6cdbbe8503b79226d5ba238f35c66b4815676
shockedRose <miguelj0916@gmail.com>
2019-09-22 16:13:22 -0500

    Nueva funcionalidad para guardar el tiempo en que fue editado con cada boton

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ const text = document.querySelector('input[type="text"]');
 button.addEventListener('click', () => {
     let newConcept = Concept(`<section class="concept"><h4 class="title">${text.value}</h4><textarea class="textbox"></textarea><button class="save">Save</button></section>`);
 
+    newConcept.children[2].addEventListener('click', (event) => {
+        event.target.insertAdjacentHTML( 'afterend',`<h6>${new Date}</h6>`);
+    });
     conceptsContainer.insertAdjacentElement('beforeend', newConcept);
 
 });
